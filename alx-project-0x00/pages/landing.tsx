75bc650a5846bb444a847bee2732281f89cfa378
kingsleyiwuchukwu <kingsleyiwuchukwu0@gmail.com>
2025-07-18 00:45:44 +0100

    Basic Routing

---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Card from "@/components/Card"
+//import Button from "@/components/Button";
+
+
+const LandingPage: React.FC = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Explore Villas</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <Card />
+        <Card />
+        <Card />
+        <h1 className="text-2xl font-bold mb-4">Reusable Buttons</h1>
+        
+        <div className="space-x-4">
+            <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-4 py-2 rounded-sm">Small</button>
+            <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-4 py-2 rounded-md">Medium</button>
+            <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-4 py-2 rounded-lg">Large</button>
+            <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-4 py-2 rounded-full">Full-Large</button>
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
